d98c0a8ad1a1f5e0403665518c356b8550e043e0
zxbyteman <sash-a@nm.ru>
2013-11-14 02:58:27 -0800

    Added socket creation

--- lab2/main.c
+++ lab2/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int CreateSocket(const char *protocolName)
+{
+    int socketDescriptor;
+    struct protoent *protocol;
+
+
+    protocol = getprotobyname(protocolName);
+    socketDescriptor = socket(AF_INET, SOCK_STREAM, protocol->p_proto);
+
+    if (socketDescriptor == -1)
+    {
+        printf("Socket creation failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    return socketDescriptor;
+
+}
+
 
 int main()
 {
-    printf("Hello world!\n");
+
+    int socketDescriptor = CreateSocket("tcp");
+
+    printf("%d", socketDescriptor);
+
+    close(socketDescriptor);
+
     return 0;
 }
